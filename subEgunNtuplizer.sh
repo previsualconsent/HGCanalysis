1be0120afe980e5cd043f92306598250571be8a6
Pedro Silva <psilva@cern.ch>
2014-08-19 22:21:54 +0200

    removing unused

--- subEgunNtuplizer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-a=(SingleElectron_SLHC13_50um SingleElectron_SLHC13_noB_50um SingleElectron_vSLHC13 SingleElectron_vSLHC13_noB)
-for k in ${a[@]}; do for i in `seq 0 50 450`; do python scripts/submitLocalAnalysis_cfg.py -t ${k} -q 8nh -f ${i} -s 50; done done
